c8b5930816d89921129f51a58be950b53bb8a247
hyo-eun-kim <heun7410@naver.com>
2021-04-05 00:20:11 +0900

    데이터 경로 수정
    - karpathy_json_path 수정
    - output_folder 수정

--- create_input_files.py
+++ create_input_files.py
@@ -4,9 +4,9 @@ if __name__ == '__main__':
     # Create input files (along with word map)
     # 함수 실행
     create_input_files(dataset='coco',
-                       karpathy_json_path='../../1. data/MSCOCO_Korean.json',
+                       karpathy_json_path='../../1. data/FILTERED_MSCOCO_Korean.json',
                        image_folder='../../1. data',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='../../1. data/temp',
+                       output_folder='../../1. data/json',
                        max_len=50)
