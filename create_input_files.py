533facb7db209e17b13920e354b5dd8e1a6352e3
hyo-eun-kim <heun7410@naver.com>
2021-04-04 19:41:53 +0900

    edit image path

--- create_input_files.py
+++ create_input_files.py
@@ -4,9 +4,9 @@ if __name__ == '__main__':
     # Create input files (along with word map)
     # 함수 실행
     create_input_files(dataset='coco',
-                       karpathy_json_path='../caption data/dataset_coco.json',
-                       image_folder='/media/ssd/caption data/',
+                       karpathy_json_path='../1. data/MSCOCO_Korean.json',
+                       image_folder='../1. data',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/media/ssd/caption data/',
+                       output_folder='../1. data',
                        max_len=50)
