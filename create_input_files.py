ff1f1b8964b6d106edf9f5fb1059d205b4bcad1e
hyo-eun-kim <heun7410@naver.com>
2021-04-11 01:42:43 +0900

    경로수정

--- create_input_files.py
+++ create_input_files.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     # Create input files (along with word map)
     # 함수 실행
     create_input_files(dataset='coco',
-                       karpathy_json_path='../../1. data_v2/FILTERED_MSCOCO_Korean_v2.json',
-                       image_folder='../../1. data_v2',
+                       karpathy_json_path='../../1. data/MSCOCO_Korean.json',
+                       image_folder='../../1. data',
                        captions_per_image=5,
                        min_word_freq=5,
                        output_folder='../../1. data_v2/json',
